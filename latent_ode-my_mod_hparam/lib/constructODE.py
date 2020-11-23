59e71f21b0955abbde02c13530508c9cdaa070c3
nandometzger <nando.metzger1996@gmail.com>
2020-11-23 11:22:20 +0100

    delte runs

---
+++ latent_ode-my_mod_hparam/lib/constructODE.py
@@ -0,0 +1,45 @@
+###########################
+# Crop Classification under Varying Cloud Cover with Neural Ordinary Differential Equations
+# Author: Nando Metzger
+###########################
+
+import lib.utils as utils
+
+from lib.ode_func import ODEFunc
+from lib.diffeq_solver import DiffeqSolver
+
+import torch.nn as nn
+import torch
+
+
+def get_diffeq_solver(ode_latents, ode_units, rec_layers,
+		ode_method, ode_type="linear",
+		device = torch.device("cpu"), convolutional=False,
+		ode_kernel=3):
+
+	experiment = True
+	if convolutional:
+		if experiment:
+			ode_func_net = utils.create_net(ode_latents, ode_latents, 
+			n_layers = int(rec_layers), n_units = int(ode_units), nonlinear = nn.Tanh)
+		else:
+			ode_func_net = utils.create_conv_net(ode_latents, ode_latents, 
+			n_layers = int(rec_layers), n_units = int(ode_units), nonlinear = nn.Tanh, ode_kernel=ode_kernel)
+	else:
+			
+		if ode_type=="linear":
+			ode_func_net = utils.create_net(ode_latents, ode_latents, 
+			n_layers = int(rec_layers), n_units = int(ode_units), nonlinear = nn.Tanh)
+		elif ode_type=="gru":
+			ode_func_net = FullGRUODECell_Autonomous(ode_latents, bias=True)
+		else:
+			raise Exception("Invalid ODE-type. Choose linear or gru.")
+
+	rec_ode_func = ODEFunc( input_dim = 0, latent_dim = ode_latents,
+		ode_func_net = ode_func_net, device = device).to(device)
+
+	z0_diffeq_solver = DiffeqSolver(0, rec_ode_func, ode_method, ode_latents, 
+		odeint_rtol = 1e-3, odeint_atol = 1e-4, device = device, convolutional=convolutional, experiment=experiment)
+
+	return z0_diffeq_solver
+
